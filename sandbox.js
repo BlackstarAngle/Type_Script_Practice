a536b7568c69e6950a64f2aa506447da6e015f0c
Mateusz <mati5033@wp.pl>
2024-01-17 17:35:18 +0100

    anyType

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,25 @@
-//arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-//names.push(4);
-//names[0] = 3;
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-//numbers.push('shaun');
-//numbers[1] = 'shaun';
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-//objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
+var age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = {
+    name: 'luigi'
 };
-ninja.age = 40;
-ninja.name = 'ryu';
-//ninja.age = '30';
-//ninja.skills = ['fighting', 'sneaking'];
+console.log(age);
+var mixed = [];
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+var ninja;
 ninja = {
     name: 'yoshi',
-    belt: 'orange'
-    //age: 40,
-    //skills: []
+    age: 25
 };
+console.log(ninja);
+ninja = {
+    name: 25,
+    age: 'yoshi'
+};
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,35 @@
-//arrays
-let names = ['luigi', 'mario', 'yoshi'];
 
-names.push('toad');
-//names.push(4);
-//names[0] = 3;
+let age: any = 25;
 
-let numbers = [10, 20, 30, 40];
-
-numbers.push(25);
-//numbers.push('shaun');
-//numbers[1] = 'shaun';
-
-let mixed = ['ken', 4, 'chun-li', 8, 9];
-
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-
-//objects
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = {
+    name: 'luigi'
 };
+console.log(age);
 
-ninja.age = 40;
-ninja.name = 'ryu';
-//ninja.age = '30';
-//ninja.skills = ['fighting', 'sneaking'];
+let mixed: any[] = [];
+
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+
+let ninja: {
+    name: any,
+    age: any
+};
 
 ninja = {
     name: 'yoshi',
-    belt: 'orange'
-    //age: 40,
-    //skills: []
-}
+    age: 25
+};
+console.log(ninja);
+
+ninja = {
+    name: 25,
+    age: 'yoshi'
+};
+console.log(ninja);
