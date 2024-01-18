b8ff31e9bba68fdabd752f0be0180f616524827f
Mateusz <mati5033@wp.pl>
2024-01-18 12:18:04 +0100

    Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('hello world1');
+let greet;
+//greet = 'hello';
+greet = () => {
+    console.log('hello, again');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+//result = 'something else';
